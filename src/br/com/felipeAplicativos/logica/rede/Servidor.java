728be6f12cb6e06584ca95f5a12ef6972e27aabe
felipexpert <felipe.miquilini@gmail.com>
2014-11-26 13:43:15 -0200

    minor changes

--- src/br/com/felipeAplicativos/logica/rede/Servidor.java
+++ src/br/com/felipeAplicativos/logica/rede/Servidor.java
@@ -12,13 +12,13 @@ public class Servidor {
 	private List<ObjectOutputStream> oosList = new ArrayList<>();
 	
 	public Servidor() throws IOException {
-		ServerSocket ss = new ServerSocket(5000);
-		
-		while(true) {
-			Socket s = ss.accept();
-			
-			ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
-			oosList.add(oos);
+		try (ServerSocket ss = new ServerSocket(5000)) {
+			while(true) {
+				Socket s = ss.accept();
+				
+				ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+				oosList.add(oos);
+			}
 		}
 	}
 }
